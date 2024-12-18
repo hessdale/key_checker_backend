6af84f715c8e94c02bb501583b0a46cd963474f5
hessdale <hessdale96@gmail.com>
2024-12-17 21:16:22 -0800

    added basic cclient log in funcctions from another project

--- app.py
+++ app.py
@@ -5,6 +5,64 @@ from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
+@app.post("/api/client")
+def post_client():
+    try:
+        error = dbhelper.check_endpoint_info(request.json,["username","password"])
+        if(error != None):
+            return make_response(jsonify(error),400)
+        salt=uuid4().hex
+        results = dbhelper.run_procedure("call post_client(?,?,?)",[request.json.get("username"),request.json.get("password"),salt])
+        if(type(results) == list):
+            return make_response(jsonify(results),200)
+        else:
+            return make_response("sorry something went wrong",500)
+    # some except blocks with possible errors
+    except TypeError:
+        print("invalid input type, try again.")
+    except UnboundLocalError:
+        print("coding error")
+    except ValueError:
+        print("value error, try again") 
+
+@app.post("/api/client-login")
+def post_login():
+    try:
+        error = dbhelper.check_endpoint_info(request.json,["username","password"])
+        if(error != None):
+            return make_response(jsonify(error),400)
+        token=uuid4().hex
+        results = dbhelper.run_procedure("call post_client_session(?,?,?)",[token,request.json.get("username"),request.json.get("password")])
+        if(type(results) == list):
+            return make_response(jsonify(results),200)
+        else:
+            return make_response("sorry something went wrong",500)
+    # some except blocks with possible errors
+    except TypeError:
+        print("invalid input type, try again.")
+    except UnboundLocalError:
+        print("coding error")
+    except ValueError:
+        print("value error, try again") 
+
+@app.delete("/api/client-login")
+def delete_login():
+    try:
+        error = dbhelper.check_endpoint_info(request.headers,["token"])
+        if(error != None):
+            return make_response(jsonify(error),400)
+        results = dbhelper.run_procedure("call delete_client_session(?)",[request.headers.get("token")])
+        if(type(results) == list):
+            return make_response(jsonify(results),200)
+        else:
+            return make_response("sorry something went wrong",500)
+    # some except blocks with possible errors
+    except TypeError:
+        print("invalid input type, try again.")
+    except UnboundLocalError:
+        print("coding error")
+    except ValueError:
+        print("value error, try again") 
 
 
 
